4f5eec2d139752aefa6338f121ef7ee1bf6fbece
ysraelmoreno <ysraelmoreno02@gmail.com>
2020-09-02 04:30:35 -0300

    All tests working.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,11 +1,19 @@
 import Transaction from '../models/Transaction';
 
 interface Balance {
+  // Entrada de dinheiro
   income: number;
+  // Saída de dinheiro
   outcome: number;
+  // Total
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +22,56 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
+  /**
+   * Essa rota deve retornar uma listagem com todas as
+   * transações que você cadastrou até agora, junto com
+   * o valor de soma de entradas, retiradas e total de crédito.
+   *
+   * [] Listagem de todas as transações
+   * [] Cálculo de Gastos
+   * [] Cálculo de Ganhos
+   * [] Cálculo total
+   */
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
